1f2e8b7696335bb9dd1a9f23a5df3d30b6e7a886
ondrejhonus <honusondrej@gmail.com>
2025-02-01 14:00:35 +0100

    cleanup main

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import pygame
-from models.settings import *
-from models.player import Player
-from models.sec_player import SecondPlayer
-from models.borders import WorldBorder
 from models.game import Game
 
 if __name__ == "__main__":
